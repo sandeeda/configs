ab875eb3ce97b764c41849f661ae22948f5d9a8c
Das <sandeep.a.das@capgemini.com>
2018-10-12 07:00:14 -0400

    first commit

---
+++ src/main/java/com/capgemini/configclient/controller/MessageResourceController.java
@@ -0,0 +1,24 @@
+package com.capgemini.configclient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RefreshScope
+@RestController
+public class MessageResourceController {
+
+	
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	@Value("${message: Hello}")
+	private String message;
+
+	@GetMapping("/message")
+	public String getMessage()
+	{
+		return message;
+	}
+}
